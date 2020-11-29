367151e2a3ca9d9626a11d32f73df17263202f14
DESKTOP-4R3QEPO\miros <miroslav.svetoslavov.slavchev@gmail.com>
2020-11-29 12:01:29 +0200

    add interfaces for produts

---
+++ client/src/app/models/product.ts
@@ -0,0 +1,9 @@
+export interface IProduct {
+    id: number;
+    name: string;
+    description: string;
+    pictureUrl: string;
+    productType: string;
+    productBrand: string;
+    price: number;
+  }
