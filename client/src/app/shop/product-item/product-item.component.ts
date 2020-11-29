c79340a4e3eaae12b80cdd9b18d3ef87565e4c00
DESKTOP-4R3QEPO\miros <miroslav.svetoslavov.slavchev@gmail.com>
2020-11-29 14:48:00 +0200

    pass data to child component

--- client/src/app/shop/product-item/product-item.component.ts
+++ client/src/app/shop/product-item/product-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/shared/models/product';
 
 @Component({
   selector: 'app-product-item',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductItemComponent implements OnInit {
 
+  @Input() product!: IProduct;
   constructor() { }
 
   ngOnInit(): void {
--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,23 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { IBrand } from '../shared/models/brand';
 import { IProduct } from '../shared/models/product';
+import { IType } from '../shared/models/productType';
 import { ShopService } from './shop.service';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.scss']
+  styleUrls: ['./shop.component.scss'],
 })
 export class ShopComponent implements OnInit {
   products!: IProduct[];
+  brands!: IBrand[];
+  types!: IType[];
 
-  constructor(private shopService: ShopService) { }
+  constructor(private shopService: ShopService) {}
 
   ngOnInit(): void {
-    this.shopService.getProducts().subscribe(response => {
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    });
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
   }
 
+  getProducts(): void {
+    this.shopService.getProducts().subscribe(
+      (response) => {
+        this.products = response.data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getBrands(): void {
+    this.shopService.getBrands().subscribe(
+      (response) => {
+        this.brands = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getTypes(): void {
+    this.shopService.getTypes().subscribe(
+      (response) => {
+        this.types = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
