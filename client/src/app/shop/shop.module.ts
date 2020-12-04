2cb23a79e9d72ed3cf5281ac82888adda89576b7
DESKTOP-4R3QEPO\miros <miroslav.svetoslavov.slavchev@gmail.com>
2020-12-04 14:50:41 +0200

    Extract paging header into component

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
+import { PagingHeaderComponent } from '../shared/components/paging-header/paging-header.component';
 
 
 
