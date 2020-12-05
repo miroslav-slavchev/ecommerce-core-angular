681fb06f69a343c6814c46ec6959f0361f10c53b
DESKTOP-4R3QEPO\miros <miroslav.svetoslavov.slavchev@gmail.com>
2020-12-05 12:02:09 +0200

    Implement product details component

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/shared/models/product';
+import { ShopService } from '../shop.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
 })
 export class ProductDetailsComponent implements OnInit {
+  product!: IProduct;
 
-  constructor() { }
+  constructor(
+    private shopService: ShopService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.loadProduct();
   }
 
+  loadProduct(): void {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id) {
+      this.shopService.getProduct(+id).subscribe(
+        (product) => {
+          this.product = product;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+    else{
+      console.log(id + ' not found');
+    }
+  }
 }
