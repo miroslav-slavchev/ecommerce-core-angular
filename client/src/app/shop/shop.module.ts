5390035e89fde2aee9a43631200e77de08aad12a
DESKTOP-4R3QEPO\miros <miroslav.svetoslavov.slavchev@gmail.com>
2020-12-05 10:05:33 +0200

    Create product details component

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -4,11 +4,12 @@ import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
 import { PagingHeaderComponent } from '../shared/components/paging-header/paging-header.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
 
 @NgModule({
-  declarations: [ShopComponent, ProductItemComponent],
+  declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
   imports: [
     SharedModule,
     CommonModule
